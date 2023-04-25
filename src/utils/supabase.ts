0bc49433191698f56fb0bc33c028cd89b1efa08f
Derek Rodriguez <drod2169@gmail.com>
2023-04-25 05:33:17 -0400

    Add db type

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,10 @@
-import { createClient } from "@supabase/supabase-js";
+import { type SupabaseClient, createClient } from "@supabase/supabase-js";
+import type { Database } from '~/types/supabase';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? "";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+type Client = SupabaseClient<Database, 'public'>;
+
+export const supabase: Client = createClient<Database>(supabaseUrl, supabaseKey);
 export default supabase;
