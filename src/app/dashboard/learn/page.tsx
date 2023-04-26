d3772da2b207a251f1df565b98ecf61419564432
Derek Rodriguez <drodriguez@hubspot.com>
2023-04-26 07:27:33 -0400

    add todo

--- src/app/dashboard/learn/page.tsx
+++ src/app/dashboard/learn/page.tsx
@@ -1,5 +1,6 @@
 import Learn from "./learn";
 
 export default function LearnPage() {
+  // TODO: Fetch categories, subjects, contentTypes
   return <Learn categories={[]} subjects={[]} contentTypes={[]} />;
 }
