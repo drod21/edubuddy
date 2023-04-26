a27635db9aaa6598e71aca804cef78a4f3df3753
Derek Rodriguez <drodriguez@hubspot.com>
2023-04-26 11:07:25 -0400

    Update to user table

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -6,13 +6,25 @@ import ProfileForm from "./ProfileForm";
 export const revalidate = 0;
 
 export default async function ProfilePage() {
+  const user = auth().user;
   const token = (await auth().getToken({ template: "supabase" })) ?? "";
   await supabase.auth.setSession({ access_token: token, refresh_token: "" });
   const userMetadata = await supabase
-    .from("user_metadata")
+    .from("user")
     .select()
     .eq("user_id", auth().userId)
     .single();
+
+  if (!userMetadata && user) {
+    await supabase.from("user").insert({
+      id: user.id,
+      email:
+        user.emailAddresses?.find((e) => e.id === user.primaryEmailAddressId)
+          ?.emailAddress ?? "",
+      firstName: user.firstName ?? "",
+      lastName: user.lastName ?? "",
+    });
+  }
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
