16818e577e51b728102002bd0e1e472efbdf9ff3
Derek Rodriguez <drodriguez@hubspot.com>
2023-04-27 07:02:15 -0400

    type cleanup

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,10 +1,8 @@
-import { type SupabaseClient, createClient } from "@supabase/supabase-js";
-import type { Database } from '~/types/supabase';
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "~/types/supabase";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? "";
 
-type Client = SupabaseClient<Database, 'public'>;
-
-export const supabase: Client = createClient<Database>(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 export default supabase;
